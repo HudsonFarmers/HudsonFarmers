2d6f2fbd059f44d3fea3829acb72790766752b3f
FatimaUppal <125939989+FatimaUppal@users.noreply.github.com>
2023-03-30 16:27:42 -0400

    push

--- src/test/java/com/zipcodewilmington/froilansfarm/ChickenTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/ChickenTest.java
@@ -1,4 +1,28 @@
 package com.zipcodewilmington.froilansfarm;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ChickenTest {
+    Animal.Chicken chicken= new Animal.Chicken();
+    Tomato tomato= new Tomato();
+    EatCorn corn = new EatCorn();
+    Egg egg = new Egg();
+
+
+@Test
+    public void testInheritance(){
+        Assert.assertTre(chicken instanceof produce);
+    }
+@Test
+    public void testInheritance() {
+        Assert.assertTrue(chicken instanceof Animal);
+    }
+
+
+@Test
+public void makesNoiseTest(){
+    String expected = "bak bak";
+    Assert.assertEquals(expected, chicken.makesNoise());
+}
 }
