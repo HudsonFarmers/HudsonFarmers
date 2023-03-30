5fe93e43241d85c134d0c2b25185b8d3c062f381
Christa <christa@Zipcoders-MBP-6.fios-router.home>
2023-03-30 19:16:36 -0400

    methods

--- src/main/java/com/zipcodewilmington/froilansfarm/Crop/Crop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Crop/Crop.java
@@ -6,4 +6,9 @@ import com.zipcodewilmington.froilansfarm.edible.Edible;
 public abstract class Crop<T extends Edible> {
 
 
+    public boolean hasBeenFertilized() {
+    }
+
+    public T yield() {
+    }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Crop/TomatoPlant.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Crop/TomatoPlant.java
@@ -4,4 +4,8 @@ import com.zipcodewilmington.froilansfarm.Crop.Crop;
 import com.zipcodewilmington.froilansfarm.edible.Tomato;
 
 public class TomatoPlant extends Crop<Tomato> {
+
+    public Tomato yield() {
+        return new Tomato();
+    }
 }
