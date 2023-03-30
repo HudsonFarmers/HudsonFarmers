adc32fa1357de46b60ee31e4343e4d5e536047af
timothy <pimtarka@gmail.com>
2023-03-30 12:43:09 -0400

    finished tractor tests

--- src/test/java/com/zipcodewilmington/froilansfarm/TractorTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/TractorTest.java
@@ -1,4 +1,57 @@
 package com.zipcodewilmington.froilansfarm;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TractorTest {
+    @Test
+    public void testTractorConstructor() {
+        Tractor tractor = new Tractor();
+        Vehicle Tractor = new Tractor();
+
+        Assert.assertTrue(tractor instanceof Tractor && Tractor instanceof Vehicle);
+    }
+
+    @Test
+    public void testInheritanceNoiseMaker() {
+        Tractor tractor = new Tractor();
+
+        Assert.assertTrue(tractor instanceof NoiseMaker);
+    }
+
+    @Test
+    public void testInheritanceRideable() {
+        Tractor tractor = new Tractor();
+
+        Assert.assertTrue(tractor instanceof Rideable);
+    }
+
+    @Test
+    public void testTractorHarvest() {
+        Tractor tractor = new Tractor();
+        Pilot pilot = new Pilot();
+        CropDuster cropDuster = new CropDuster();
+
+        cropRow.add(tomato);
+        pilot.mount(cropDuster);
+        cropDuster.fly(cropRow);
+        tractor.harvest(cropRow);
+
+        Assert.assertTrue(instance.contains(tomato));
+    }
+
+    @Test
+    public void testOperate() {
+        Tractor tractor = new Tractor();
+        Pilot pilot = new Pilot();
+        CropDuster cropDuster = new CropDuster();
+
+        cropRow.add(tomato);
+        pilot.mount(cropDuster);
+        cropDuster.fly(cropRow);
+
+        tractor.operate(cropRow);
+        String expected = "Chugga Chugga";
+        Assert.assertTrue(canBeHarvested == true && expected == tractor.makeNoise());
+    }
 }
