92d6d914f82efd78e6119a62d4994def975f3f93
timothy <pimtarka@gmail.com>
2023-04-13 13:22:40 -0400

    finished product

--- src/test/java/com/zipcodewilmington/froilansfarm/VehicleTest/CropDusterTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/VehicleTest/CropDusterTest.java
@@ -1,8 +1,10 @@
 package com.zipcodewilmington.froilansfarm.VehicleTest;
 
 import com.zipcodewilmington.froilansfarm.Animal.animals.NoiseMaker;
+import com.zipcodewilmington.froilansfarm.Animal.persons.Farmer;
 import com.zipcodewilmington.froilansfarm.Animal.persons.Pilot;
 import com.zipcodewilmington.froilansfarm.Crop.CropRow;
+import com.zipcodewilmington.froilansfarm.Crop.TomatoPlant;
 import com.zipcodewilmington.froilansfarm.Vehicle.*;
 import com.zipcodewilmington.froilansfarm.farm.Field;
 import org.junit.Assert;
@@ -55,5 +57,24 @@ public class CropDusterTest {
         CropDuster cropDuster = new CropDuster();
 
         Assert.assertTrue(cropDuster instanceof AirCraft);
+    }
+    @Test
+    public void testOperate() {
+        Farmer<Tractor> farmer = new Farmer();
+        Pilot pilot = new Pilot();
+        CropDuster cropDuster = new CropDuster();
+
+        CropRow cropRow = new CropRow();
+        TomatoPlant tomatoPlant = new TomatoPlant();
+        Field field = new Field();
+        farmer.plant(tomatoPlant, cropRow);
+        field.add(cropRow);
+
+        cropRow.add(tomatoPlant);
+        pilot.mount(cropDuster);
+        cropDuster.operate(field);
+        boolean expected = true;
+
+        Assert.assertEquals(expected, tomatoPlant.isFertilized());
     }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/VehicleTest/TractorTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/VehicleTest/TractorTest.java
@@ -1,7 +1,13 @@
 package com.zipcodewilmington.froilansfarm.VehicleTest;
+
 import com.zipcodewilmington.froilansfarm.Animal.animals.NoiseMaker;
 import com.zipcodewilmington.froilansfarm.Animal.persons.Farmer;
+import com.zipcodewilmington.froilansfarm.Animal.persons.Pilot;
+import com.zipcodewilmington.froilansfarm.Crop.CropRow;
+import com.zipcodewilmington.froilansfarm.Crop.TomatoPlant;
 import com.zipcodewilmington.froilansfarm.Vehicle.*;
+import com.zipcodewilmington.froilansfarm.farm.Field;
+import com.zipcodewilmington.froilansfarm.farm.Storage;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -33,13 +39,53 @@ public class TractorTest {
 
         Assert.assertTrue(tractor instanceof FarmVehicle);
     }
+    @Test
+    public void testMounted() {
+        Farmer<Tractor> farmer = new Farmer();
+        Tractor tractor = new Tractor();
+        farmer.mount(tractor);
 
+        Assert.assertTrue(tractor.isMounted());
+    }
+    @Test
+    public void testDismount() {
+        Farmer<Tractor> farmer = new Farmer();
+        Tractor tractor = new Tractor();
+    }
     @Test
     public void testMakeNoise() {
+        Farmer<Tractor> farmer = new Farmer();
+        Tractor tractor = new Tractor();
+
+        farmer.mount(tractor);
         String expected = "Vroom";
-        String actual = new Tractor().makeNoise();
-        Assert.assertEquals(expected,actual);
+
+        Assert.assertEquals(expected, tractor.makeNoise());
     }
 
+    @Test
+    public void testTractorOperate() {
+        Farmer<Tractor> farmer = new Farmer();
+        Tractor tractor = new Tractor();
+
+        Pilot pilot = new Pilot();
+        CropDuster cropDuster = new CropDuster();
+
+        CropRow cropRow = new CropRow();
+        TomatoPlant tomatoPlant = new TomatoPlant();
+        Field field = new Field();
+        farmer.plant(tomatoPlant, cropRow);
+        field.add(cropRow);
+
+        cropRow.add(tomatoPlant);
+        pilot.mount(cropDuster);
+        cropDuster.operate(field);
+        farmer.mount(tractor);
+        tractor.operate(field);
+        int expected = 6;
+
+        Assert.assertEquals(expected, Storage.getTomatoStorage().size());
+    }
 
 }
+
